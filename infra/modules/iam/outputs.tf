aeee5924487d64cc943658e9e76f1f6095c57c00
Birendra Singh <birendra@DESKTOP-LPV9HSD.localdomain>
2026-05-11 05:02:22 +0000

    fix: remove .terraform provider binaries from git tracking

--- infra/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "eks_cluster_role_arn" {
-  description = "EKS cluster IAM role ARN"
-  value       = aws_iam_role.eks_cluster.arn
-}
-
-output "eks_node_group_role_arn" {
-  description = "EKS node group IAM role ARN"
-  value       = aws_iam_role.eks_node_group.arn
-}
